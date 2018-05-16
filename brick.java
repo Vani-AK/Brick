8f5a468c11ebb1e4cf1d13aca8da3e5e64c62026
Vani-AK <vani.anilkumar@gmail.com>
2018-05-16 08:43:22 -0700

    Update brick.java

--- brick.java
+++ brick.java
@@ -1,7 +1,6 @@
-public class brick {
-	public static void main (String a[]){
-		System.out.println("Hello"); 
-		int a= 5; 
-		System.out.print(a);
+public class arkanoid {
+	public static void main (String [] args){
+		
+		
 	}
 }
